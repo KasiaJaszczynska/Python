315c1f6b61e80981ea6bb1e504a8341224a642c7
jaszczynaka.k@gmail.com <you@example.com>
2022-09-29 12:02:29 +0200

    .

---
+++ zadania.py
@@ -0,0 +1,19 @@
+print("Hello word") #to jest komentarz
+
+zmienna=10
+print(zmienna)
+
+print ("napis")
+print("napis1")
+print("napis2")
+
+print("Wartosci zmiennej to:")
+print (zmienna)
+
+print("Wartosc zminna to ", zmienna)
+print("wartosc zminnej to: %.2f" %zmienna)
+print(f"wartosc zmiennej to {zmienna}")
+print("Wartosc zmiennej to :"+ str(zmienna))
+
+
+wiek=int(input("ile masz lat")) #int ogramiczemie dlaytkownika moze podac tylko liczbe nie slownie
