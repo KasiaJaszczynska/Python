bab583d12b57692e1dcc3d9568bd78c3a64beffc
jaszczynaka.k@gmail.com <you@example.com>
2022-10-11 11:38:09 +0200

    kasia

--- zadania.py
+++ zadania.py
@@ -1,19 +1,80 @@
-print("Hello word") #to jest komentarz
+import random
 
-zmienna=10
-print(zmienna)
+#print("Hello word") #to jest komentarz
 
-print ("napis")
-print("napis1")
-print("napis2")
+#zmienna=10
+#print(zmienna)
 
-print("Wartosci zmiennej to:")
-print (zmienna)
+#print ("napis")
+#print("napis1")
+#print("napis2")
 
-print("Wartosc zminna to ", zmienna)
-print("wartosc zminnej to: %.2f" %zmienna)
-print(f"wartosc zmiennej to {zmienna}")
-print("Wartosc zmiennej to :"+ str(zmienna))
+#print("Wartosci zmiennej to:")
+#print (zmienna)
+
+#print("Wartosc zminna to ", zmienna)
+#print("wartosc zminnej to: %.2f" %zmienna)
+#print(f"wartosc zmiennej to {zmienna}")
+#print("Wartosc zmiennej to :"+ str(zmienna))
 
 
-wiek=int(input("ile masz lat")) #int ogramiczemie dlaytkownika moze podac tylko liczbe nie slownie
+#wiek=int(input("ile masz lat")) #int ograniczemie dla tkownika moze podac tylko liczbe nie slownie
+
+
+#suma=2+3
+#roznica=4-2
+#iloczyn=2*4
+#iloraz=4/2
+
+#reszta=5%2
+#potega=2**4
+
+ciag="napis"*10
+
+print(ciag)
+
+los=random.randint(1, 100)
+print(los)
+a=10
+b=20
+
+los1=random.randint(a, b)
+print(los1)
+
+
+if a>0:
+    print("Zmienna jest wieksza od 0")
+
+elif a==0:
+    print("Zmienna jest równa 0")
+
+else:
+    print("Zmienna jest mniejsza od 0")
+
+
+#los1=int(input("Podaj liczbę"))
+#if a>0:
+ #   print("Zmienna jest większa od 0 ")
+#else:
+ #   print("Zmienna jest miejsza od0 ")
+
+
+#los1=int(input("Podaj licznę"))
+#if a%2==0:
+ #   print("Zmienna jest podzielna przez 2")
+
+#else:    
+ #   print("Zmienna nie jest podzielna przez 2 ")
+
+
+
+a=int(input("Podaj liczbę"))
+b=int(input("Podaj liczbę"))
+c=int(input("Podaj liczbę"))
+if a or b or c >0:
+    print("Zmienna jest większa od 0")
+else:
+    print("Zmienna jest mniejsza od 0")
+
+
+
