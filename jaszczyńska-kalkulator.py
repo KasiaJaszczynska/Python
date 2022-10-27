ce3ebb4300eb73d8846face860471fd9d2759696
jaszczynaka.k@gmail.com <you@example.com>
2022-10-27 12:12:49 +0200

    kd

---
+++ jaszczyńska-kalkulator.py
@@ -0,0 +1,40 @@
+def dodaj(a, b):
+    return a+b
+
+a=int(input("Podaj pierwszą liczbę:"))
+b=int(input("Podaj drugą liczbę:"))
+
+dzialanie=input("Jakie działanie chcesz wykonać?  + dodawanie, - odjemowanie, * mnoenie, / dzielnik, ^ potęgowanie")
+
+if dzialanie=="+":
+    wynik=a+b
+    print("Wynik dodawania to:", wynik)
+elif dzialanie=="-":
+    wynik=a-b
+    print("Wynik odejmowania to:", wynik)
+elif dzialanie=="*":
+    wynik=a*b
+    print("Wynik mnoenia to:", wynik)
+elif dzialanie=="/":
+    while b==0:
+        print("Nie dziel przez 0")
+        b=int(input("Podaj liczbę jecze raz :"))
+    wynik=a/b
+    print("Wynik dzielenia to:", wynik)
+
+    #if b==0:
+     #   print("Nie mona dzielić przez 0")
+    #else:
+     #   wynik=a/b
+      #  print("Wynik dzielenia to:", wynik)
+elif dzialanie=="^":
+    wynik=a**b
+    print("Wynik potęgowania to:", wynik)
+else:
+    print("Niewłaściwy znak")
+
+
+
+
+    
+
