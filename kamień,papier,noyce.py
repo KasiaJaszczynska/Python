19930ee177b3026995a01e6dc598d41f2e1b8088
jaszczynaka.k@gmail.com <you@example.com>
2022-11-10 12:09:14 +0100

    dh

--- kamień,papier,noyce.py
+++ kamień,papier,noyce.py
@@ -1,54 +1,72 @@
 import random
-name=input("Podaj imię:")
+import time
+import sys
+name=(input("Podaj imię:"))
 
+print("Jeśli chesz zakończy grę klikni 'k' ")
+time.sleep(0.5)
 
+while True:
+    print()
 
-o="kamień"
-p="papier"
-y="nozyce"
-k="koniec gry"   #gracz ybiera aby zakończyć grę
+    wybor=int(input("Wybierz: o- kamień,p papier , y- nozyce"))
+    wybor_komp = random.randrange(o,p,y )
+    o="kamień"
+    p="papier"
+    y="nozyce"
 
-
-choose=("kamień", "papier", "nozyce")
-
-while play:
-    os.system("clear")
-    print(intro)
-
+    print("3..")
+    time.sleep(0.5)
+    print("2..")
+    time.sleep(0.5)
+    print("1..")
+    time.sleep(0.5)
 
     if wybor== o and wybor== o:
-    print("o==o")
-    print("Remis")
+        print("o==o")
+        time.sleep(0.5)
+        print("Remis")
     elif wybor== o and wybor== y:
-    print("o>y")
-    print("Wygrywasz")
+        print("o>y")
+        time.sleep(0.5)
+        print("Wygrywasz")
     elif wybor== o and wybor== p:
-    print("o<p")
-    print("Przegrywasz")
+        print("o<p")
+        time.sleep(0.5)
+        print("Przegrywasz")
     elif wybor==k:
-    print("Koniec gry")
+        print("Koniec gry")
 
     if wybor==p and wybor==p:
-    print("p==p")
-    print("Remis")
+        print("p==p")
+        time.sleep(0.5)
+        print("Remis")
     elif wybor== p and wybor== o:
-    print("p>o")
-    print("Wygrywasz")
+        print("p>o")
+        time.sleep(0.5)
+        print("Wygrywasz")
     elif wybor== p and wybor== y:
-    print("p<y")
-    print("Przegrywasz")
+        print("p<y")
+        time.sleep(0.5)
+        print("Przegrywasz")
     elif wybor==k:
-    print("Koniec gry")
+        print("Koniec gry")
 
     if wybor==y and wybor==y:
-    print("y==y")
-    print("Remis")
+        print("y==y")
+        time.sleep(0.5)
+        print("Remis")
     elif wybor== y and wybor== p:
-    print("y<p")
-    print("Wygrywasz")
+        print("y<p")
+        time.sleep(0.5)
+        print("Wygrywasz")
     elif wybor== y and wybor== o:
-    print("y<o")
-    print("Przegrywasz")
+        print("y<o")
+        time.sleep(0.5)
+        print("Przegrywasz")
     elif wybor==k:
-    print("Koniec gry")
+        print("Koniec gry")
 
+    print()
+    input("Wciśni  k jeśli chesz kontynuować")
+         
