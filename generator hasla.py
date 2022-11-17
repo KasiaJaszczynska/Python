f990a43bb59a57aa1ba4703036f98c83e789bc71
jaszczynaka.k@gmail.com <you@example.com>
2022-11-17 12:19:19 +0100

    ij

--- generator hasla.py
+++ generator hasla.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generuj_haslo (size= 7, chars=string.ascii_lettters + string.digits +string.punctuation ):
+def generuj_haslo (size= 7, chars=string.ascii_letters + string.digits +string.punctuation ):
     return ''.join(random.choice(chars) for _ in range(size))
 
 haslo= int(input("Jak długie ma być hasło"))
