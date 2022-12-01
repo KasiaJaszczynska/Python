eb4d88f9214893f677e4da6863780b73908e1db2
jaszczynaka.k@gmail.com <you@example.com>
2022-12-01 12:00:57 +0100

    kasia

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,11 @@
+import math
 def dodaj(a, b):
     return a+b
 
+dzialanie=input("Jakie działanie chcesz wykonać?  + dodawanie, - odjemowanie, * mnoenie, / dzielnik, ^ potęgowanie, a ** (1/stopien) pierwiastek")
+
 a=int(input("Podaj pierwszą liczbę:"))
 b=int(input("Podaj drugą liczbę:"))
-
-dzialanie=input("Jakie działanie chcesz wykonać?  + dodawanie, - odjemowanie, * mnoenie, / dzielnik, ^ potęgowanie")
 
 if dzialanie=="+":
     wynik=a+b
@@ -27,10 +28,17 @@ elif dzialanie=="/":
     #else:
      #   wynik=a/b
       #  print("Wynik dzielenia to:", wynik)
+
+#elif dzialanie =="** (1/stopien)":
+ #   wynik= a ** (1/b)
+  #  print("Wynik działania to:", wynik)
+
 elif dzialanie=="^":
     wynik=a**b
     print("Wynik potęgowania to:", wynik)
 else:
     print("Niewłaściwy znak") 
 
-
+def pierwiastek(a, stopien):
+    wynik = a ** (1 / stopien)
+    return wynik
